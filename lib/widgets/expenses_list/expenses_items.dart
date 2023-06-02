5e1fa370edfc5f53e895ee036724d7a68f819ca3
ashfaqahmad7866 <ashfaq25322@gmail.com>
2023-06-02 17:04:50 +0500

    Added a demo card with title, icon, and date

---
+++ lib/widgets/expenses_list/expenses_items.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expenses_managing_app/models/expense.dart';
+
+class ExpensesItems extends StatelessWidget {
+  const ExpensesItems({super.key, required this.expense});
+  final Expense expense;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+            child: Text(expense.title),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text('\$${expense.amount.toString()}'),
+              ),
+              const Spacer(),
+              Icon(categoryIcons[expense.category]),
+              const SizedBox(
+                width: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Text(
+                  expense.getFormatter,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}

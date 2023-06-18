181ffbf58201f313c1439774a6aa5c2b1665995e
ashfaqahmad7866 <ashfaq25322@gmail.com>
2023-06-18 20:55:59 +0500

    removing list item on swiping and showing alert message and undo message on item

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses_managing_app/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.primaryContainer,
+              foregroundColor: kColorScheme.primaryContainer)),
+      home: const Expenses(),
     ),
   );
 }
